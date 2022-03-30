bfdb377c94df6ba8ac8480735b92606914542cd0
Spring Zhang <spring.okay@gmail.com>
2022-03-30 14:38:01 +0800

    Added ICO project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0D7e183c94abAD4bbfCaa7D720b0FF0Fe6C873fd";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
